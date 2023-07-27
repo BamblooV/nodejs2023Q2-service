f023b5dc6c80cb8c3f1087d6b612e330a22971d8
Egor Gavrilenko <bambloo769@gmail.com>
2023-07-27 22:12:26 +0300

    feat: update packeges and bootstrap project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService: ConfigService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
+  console.log(`Server started on port ${configService.get('PORT')}`);
 }
 bootstrap();
