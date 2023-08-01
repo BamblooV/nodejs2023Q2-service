4cc0292cbd3e4b8f55d78b21311dfb1ab867dc39
Egor Gavrilenko <bambloo769@gmail.com>
2023-08-01 12:43:52 +0300

    fix: change favorites interface filename

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -3,7 +3,7 @@ import { Album } from '../api/album/interface/album.interface';
 import { Artist } from '../api/artist/interface/artist.interface';
 import { Track } from '../api/track/interface/track.interface';
 import { UserEntity } from '../api/user/entities/user.entity';
-import { Favorites } from '../api/favorites/interface/favotites.interface';
+import { Favorites } from '../api/favorites/interface/favorites.interface';
 import { DBNotFound } from '../common/errors';
 
 export const enum DBEntities {
