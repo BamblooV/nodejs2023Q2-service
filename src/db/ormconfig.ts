44df318a4bf18add70314249ef214eb552750037
Egor Gavrilenko <bambloo769@gmail.com>
2023-08-02 17:26:28 +0300

    feat: prepare typeorm config

---
+++ src/db/ormconfig.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenv.config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT) | 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: ['dist/**/entities/*entity.js'],
+  migrations: ['dist/**/migrations/*.js'],
+  logging: false,
+  migrationsRun: false,
+  synchronize: false,
+};
+
+export const dataSource = new DataSource(dataSourceOptions);
+
+dataSource.initialize();
