5f98bba24ffbc675602ba67a9f2a7b299cba226d
Egor Gavrilenko <bambloo769@gmail.com>
2023-08-13 19:26:47 +0300

    fix: update migration

---
+++ src/db/migrations/1691943165881-init.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1691943165881 implements MigrationInterface {
+    name = 'Init1691943165881'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "artist" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "grammy" boolean NOT NULL, CONSTRAINT "PK_55b76e71568b5db4d01d3e394ed" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "album" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "year" integer NOT NULL, "artistId" uuid, CONSTRAINT "PK_58e0b4b8a31bb897e6959fe3206" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "artist-fav" ("id" SERIAL NOT NULL, "artistId" uuid, CONSTRAINT "REL_fbd0effda2c010ffc44e2dd41f" UNIQUE ("artistId"), CONSTRAINT "PK_408c2feebcb4c4909999f217c2a" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "album-fav" ("id" SERIAL NOT NULL, "albumId" uuid, CONSTRAINT "REL_3dfad60b6e7e20f210ee549081" UNIQUE ("albumId"), CONSTRAINT "PK_1d5e7fc32041e8818e2666d29ec" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "track" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "artistId" uuid, "albumId" uuid, "duration" integer NOT NULL, CONSTRAINT "PK_0631b9bcf521f8fab3a15f2c37e" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "track-fav" ("id" SERIAL NOT NULL, "trackId" uuid, CONSTRAINT "REL_1076fb71a25ea513d08cc7f11b" UNIQUE ("trackId"), CONSTRAINT "PK_db145e07251886cb8a6fa09f1a0" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "login" character varying NOT NULL, "password" character varying NOT NULL, "version" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_2d443082eccd5198f95f2a36e2c" UNIQUE ("login"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "album" ADD CONSTRAINT "FK_3d06f25148a4a880b429e3bc839" FOREIGN KEY ("artistId") REFERENCES "artist"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "artist-fav" ADD CONSTRAINT "FK_fbd0effda2c010ffc44e2dd41fc" FOREIGN KEY ("artistId") REFERENCES "artist"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "album-fav" ADD CONSTRAINT "FK_3dfad60b6e7e20f210ee5490812" FOREIGN KEY ("albumId") REFERENCES "album"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "track" ADD CONSTRAINT "FK_997cfd9e91fd00a363500f72dc2" FOREIGN KEY ("artistId") REFERENCES "artist"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "track" ADD CONSTRAINT "FK_b105d945c4c185395daca91606a" FOREIGN KEY ("albumId") REFERENCES "album"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "track-fav" ADD CONSTRAINT "FK_1076fb71a25ea513d08cc7f11be" FOREIGN KEY ("trackId") REFERENCES "track"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "track-fav" DROP CONSTRAINT "FK_1076fb71a25ea513d08cc7f11be"`);
+        await queryRunner.query(`ALTER TABLE "track" DROP CONSTRAINT "FK_b105d945c4c185395daca91606a"`);
+        await queryRunner.query(`ALTER TABLE "track" DROP CONSTRAINT "FK_997cfd9e91fd00a363500f72dc2"`);
+        await queryRunner.query(`ALTER TABLE "album-fav" DROP CONSTRAINT "FK_3dfad60b6e7e20f210ee5490812"`);
+        await queryRunner.query(`ALTER TABLE "artist-fav" DROP CONSTRAINT "FK_fbd0effda2c010ffc44e2dd41fc"`);
+        await queryRunner.query(`ALTER TABLE "album" DROP CONSTRAINT "FK_3d06f25148a4a880b429e3bc839"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TABLE "track-fav"`);
+        await queryRunner.query(`DROP TABLE "track"`);
+        await queryRunner.query(`DROP TABLE "album-fav"`);
+        await queryRunner.query(`DROP TABLE "artist-fav"`);
+        await queryRunner.query(`DROP TABLE "album"`);
+        await queryRunner.query(`DROP TABLE "artist"`);
+    }
+
+}
