29d4e23ff21f206e3fd4c5c6caf888706bf5e76d
Egor Gavrilenko <bambloo769@gmail.com>
2023-08-17 23:38:39 +0300

    fix: regenerate migration

--- src/db/migrations/1692299733682-init.ts
+++ src/db/migrations/1692299733682-init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Init1691943165881 implements MigrationInterface {
-    name = 'Init1691943165881'
+export class Init1692299733682 implements MigrationInterface {
+    name = 'Init1692299733682'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "artist" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "grammy" boolean NOT NULL, CONSTRAINT "PK_55b76e71568b5db4d01d3e394ed" PRIMARY KEY ("id"))`);
