dfeaacec32d5ed49d631aadd0661d75b0bba82c7
Egor Gavrilenko <bambloo769@gmail.com>
2023-08-17 23:52:25 +0300

    feat: implement password hashing

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -7,6 +7,9 @@ import {
 } from '../../common/errors';
 import { UserEntity } from './entities/user.entity';
 import { DbService } from '../../db/db.service';
+import bcrypt from 'bcryptjs';
+
+const SALT_ROUNDS = +process.env.CRYPT_SALT;
 
 @Injectable()
 export class UserService {
@@ -14,6 +17,10 @@ export class UserService {
 
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
     const user = this.db.userRepository.create(createUserDto);
+
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+
+    user.password = hash;
 
     return await this.db.userRepository.save(user);
   }
@@ -39,12 +46,19 @@ export class UserService {
   async update(id: string, updateUserDto: UpdateUserDto): Promise<UserEntity> {
     const user = await this.findOne(id);
 
-    if (user.password !== updateUserDto.oldPassword) {
+    const isValidPassword = await bcrypt.compare(
+      user.password,
+      updateUserDto.oldPassword,
+    );
+
+    if (!isValidPassword) {
       throw new ForbiddenOperationError();
     }
 
+    const hash = await bcrypt.hash(updateUserDto.newPassword, SALT_ROUNDS);
+
     await this.db.userRepository.update(id, {
-      password: updateUserDto.newPassword,
+      password: hash,
     });
 
     return await this.findOne(id);
