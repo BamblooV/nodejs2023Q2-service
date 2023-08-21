e2e635c63e5b428edc1e9a22d1c19475ad9e1e20
Egor Gavrilenko <bambloo769@gmail.com>
2023-08-21 21:26:43 +0300

    fix: fix middleware res logger

--- src/common/middlewares/Logger.middleware.ts
+++ src/common/middlewares/Logger.middleware.ts
@@ -7,14 +7,16 @@ export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly logger: LoggingService) {}
   use(req: Request, res: Response, next: NextFunction) {
     const { baseUrl, body, params } = req;
-    const { statusCode } = res;
+    res.on('finish', () => {
+      const { statusCode } = res;
 
-    this.logger.log(
-      `Request: URL: ${baseUrl} \t PARAMS: ${JSON.stringify(
-        params,
-      )} \t BODY: ${JSON.stringify(body)}`,
-    );
-    this.logger.log(`Response: statusCode ${statusCode}`);
+      this.logger.log(
+        `Request: URL: ${baseUrl} \t PARAMS: ${JSON.stringify(
+          params,
+        )} \t BODY: ${JSON.stringify(body)}`,
+      );
+      this.logger.log(`Response: statusCode ${statusCode}`);
+    });
 
     next();
   }
